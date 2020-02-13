087380c09017e23faaea43efc06979157326355e
unknown <arshia_nazarlotfi1376@yahoo.com>
2020-02-14 00:08:25 +0330

    updating project with collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import {Route} from 'react-router-dom'
+import {Route,Switch} from 'react-router-dom'
+import ShopPage from '../src/pages/shop/shop.component';
 
 //Switch ensures only one page is going to be shown the first matches the path
 //exact cause the path to exactly match
 //if switch or exact not use it is going to render the matching pages at same time
 // /  and /hats will be renderd at same page
 
-const Hatspage = () =>(
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+
 
 function App() {
   return (
       <div>
+        <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route  path='/hats' component={Hatspage} />
+        <Route  path='/shop' component={ShopPage} />
+        </Switch>
       </div>
   )
 }
